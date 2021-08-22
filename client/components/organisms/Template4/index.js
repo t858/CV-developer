5943d6a8c01381dd719f660e815419f727caa3c7
ezza022 <alfirman.pahlepi@gmail.com>
2021-08-22 20:17:38 +0700

    static template4

--- client/components/organisms/Template4/index.js
+++ client/components/organisms/Template4/index.js
@@ -4,12 +4,32 @@ import { Layout } from "./Layout";
 export const Template4 = () => {
   return (
     <Layout>
-      <div className="flex h-full w-full ">
-        <div className="w-1/2 z-10">
+      <div className="flex h-full w-full before:block">
+        <div className="w-1/2 flex items-center">
           <Avatar />
+          <div className="h-full pt-[308px] w-3/4 mx-auto space-y-6">
+            {leftData.map((el, index) => (
+              <div key={index} className="space-y-3">
+                <h6 className="font-semibold uppercase tracking-widest">
+                  {el.title}
+                </h6>
+                {el.data.map((l, idx) => (
+                  <div key={idx} className="space-y-1">
+                    <div>
+                      <p className="uppercase font-semibold text-[#317a8b]">
+                        {l.title}
+                      </p>
+                      <p className="uppercase">{l.subtitle}</p>
+                    </div>
+                    <p className="text-gray-500">{l.text}</p>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
         </div>
         <div className="w-1/2 flex flex-col justify-center px-4 pr-12">
-          <div className="w-full h-[75%] bg-purple-300 space-y-10">
+          <div className="w-full h-[75%] space-y-6">
             <div className="space-y-4 ">
               <h1 className="font-bold text-5xl text-[#317a8b]">
                 Alfirman Ejha Pahlepi
@@ -18,29 +38,103 @@ export const Template4 = () => {
                 Calon Programmer
               </h4>
             </div>
-            <div className="space-y-2">
-              <h6 className="font-semibold uppercase tracking-widest">
-                Kontak saya
-              </h6>
-              <p className="text-gray-500">
-                What is Lorem Ipsum Lorem Ipsum What is Lorem Ipsum Lorem
-                IpsumWhat is Lorem Ipsum Lorem IpsumWhat is Lorem Ipsum Lorem
-                Ipsum
-              </p>
-            </div>
-            <div className="space-y-2">
-              <h6 className="font-semibold uppercase tracking-widest">
-                Kontak saya
-              </h6>
-              <p className="text-gray-500">
-                What is Lorem Ipsum Lorem Ipsum What is Lorem Ipsum Lorem
-                IpsumWhat is Lorem Ipsum Lorem IpsumWhat is Lorem Ipsum Lorem
-                Ipsum
-              </p>
-            </div>
+            {rightData.map((el, index) => (
+              <div key={index} className="space-y-3">
+                <h6 className="font-semibold uppercase tracking-widest">
+                  {el.title}
+                </h6>
+                {el.data.map((l, idx) => (
+                  <div key={idx} className="space-y-1">
+                    <div>
+                      <p className="uppercase font-semibold text-[#317a8b]">
+                        {l.title}
+                      </p>
+                      <p className="uppercase">{l.subtitle}</p>
+                    </div>
+                    <p className="text-gray-500">{l.text}</p>
+                  </div>
+                ))}
+              </div>
+            ))}
           </div>
         </div>
       </div>
     </Layout>
   );
 };
+
+const leftData = [
+  {
+    title: "PROFIL SAYA",
+    data: [
+      {
+        title: "",
+        subtitle: "",
+        text: "What is Lorem Ipsum Lorem Ipsum What is Lorem Ipsum Lorem IpsumWhat is Lorem Ipsum Lorem IpsumWhat is Lorem Ipsum Lorem Ipsum",
+      },
+    ],
+  },
+  {
+    title: "AKREDITASI",
+    data: [
+      {
+        title: "",
+        subtitle: "",
+        text: "What is Lorem Ipsum Lorem Ipsum What is Lorem Ipsum Lorem IpsumWhat is Lorem Ipsum Lorem IpsumWhat is Lorem Ipsum Lorem Ipsum",
+      },
+    ],
+  },
+  {
+    title: "RIWAYAT PEKERJAAN",
+    data: [
+      {
+        title: "SMAN 2 KEJURUAN MUDA",
+        subtitle: "PELAJAR | 2014 - 2017",
+        text: "What is Lorem Ipsum Lorem Ipsum What is Lorem Ipsum Lorem IpsumWhat is Lorem Ipsum Lorem IpsumWhat is Lorem Ipsum Lorem Ipsum",
+      },
+      {
+        title: "SMAN 2 KEJURUAN MUDA",
+        subtitle: "PELAJAR | 2014 - 2017",
+        text: "What is Lorem Ipsum Lorem Ipsum What is Lorem Ipsum Lorem IpsumWhat is Lorem Ipsum Lorem IpsumWhat is Lorem Ipsum Lorem Ipsum",
+      },
+    ],
+  },
+];
+
+const rightData = [
+  {
+    title: "KONTAK SAYA",
+    data: [
+      {
+        title: "",
+        subtitle: "",
+        text: "What is Lorem Ipsum Lorem Ipsum What is Lorem Ipsum Lorem IpsumWhat is Lorem Ipsum Lorem IpsumWhat is Lorem Ipsum Lorem Ipsum",
+      },
+    ],
+  },
+  {
+    title: "KEAHLIAN UTAMA",
+    data: [
+      {
+        title: "",
+        subtitle: "",
+        text: "What is Lorem Ipsum Lorem Ipsum What is Lorem Ipsum Lorem IpsumWhat is Lorem Ipsum Lorem IpsumWhat is Lorem Ipsum Lorem Ipsum",
+      },
+    ],
+  },
+  {
+    title: "RIWAYAT PENDIDIKAN",
+    data: [
+      {
+        title: "SMAN 2 KEJURUAN MUDA",
+        subtitle: "PELAJAR | 2014 - 2017",
+        text: "What is Lorem Ipsum Lorem Ipsum What is Lorem Ipsum Lorem IpsumWhat is Lorem Ipsum Lorem IpsumWhat is Lorem Ipsum Lorem Ipsum",
+      },
+      {
+        title: "SMAN 2 KEJURUAN MUDA",
+        subtitle: "PELAJAR | 2014 - 2017",
+        text: "What is Lorem Ipsum Lorem Ipsum What is Lorem Ipsum Lorem IpsumWhat is Lorem Ipsum Lorem IpsumWhat is Lorem Ipsum Lorem Ipsum",
+      },
+    ],
+  },
+];
