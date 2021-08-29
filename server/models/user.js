1b022afb16b854b1c9353bd9d5e202a7db7b2635
ezza022 <alfirman.pahlepi@gmail.com>
2021-08-29 19:36:27 +0700

    add models

--- server/models/user.js
+++ server/models/user.js
@@ -2,16 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = Schema({
-  avatar: { type: String, require: true },
-  name: { type: String, require: true },
-  title: { type: String, require: true },
-  password: { type: String, require: false },
-  number: { type: Number, require: false },
-  address: { type: String, require: false },
-  province: { type: Object, require: false },
-  city: { type: Object, require: false },
-  district: { type: Object, require: false },
-  postal: { type: Number, require: false },
+  createdAt: { type: String, require: true },
+  userdata: { type: Object, require: true },
 });
 
 module.exports = mongoose.model("User", UserSchema);
